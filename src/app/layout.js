d97b3717a0df67eb606a146271698480cb253672
jakegreenbergbell <32340594+jakegreenbergbell@users.noreply.github.com>
2024-09-04 14:39:00 -0700

    Change metadata for site. - Add favicon.

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,12 +3,13 @@ import "./globals.css";
 import StyledComponentsRegistry from "../lib/registry";
 import { JetBrains_Mono } from "next/font/google";
 import "@fontsource/jetbrains-mono";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata = {
-    title: "portfolio of Jake Greenberg-Bell",
-    description: "Jake Greenberg-Bell's portfolio website",
+    title: "Jake Greenberg-Bell",
+    description: "Jake Greenberg-Bell's personal website",
 };
 
 export default function RootLayout({ children }) {
